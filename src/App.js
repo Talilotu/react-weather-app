10333e748bafd17eb2de3932fedd45d5f78444ab
Talilotu <nesikaufusi@gmail.com>
2022-06-12 22:51:31 +1000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://app.netlify.com/sites/shiny-pie-937396/overview">
+          Akanesi Tohotoa
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Talilotu/react-weather-app">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
