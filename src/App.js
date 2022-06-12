364d5c866e035b9ad5b1ceb3328adc3e6b88e00e
Talilotu <nesikaufusi@gmail.com>
2022-06-12 23:13:09 +1000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://shiny-pie-937396.netlify.app/" target="blank">
-          Akanesi Tohotoa
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/Talilotu/react-weather-app" target="blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://shiny-pie-937396.netlify.app/" target="blank">
+            Akanesi Tohotoa
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Talilotu/react-weather-app"
+            target="blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
