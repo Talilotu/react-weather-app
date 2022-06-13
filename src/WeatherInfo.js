d76295f97e8d28caef9bb99e49b74c1e452b2cd8
Talilotu <nesikaufusi@gmail.com>
2022-06-13 22:02:17 +1000

    added react weather icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
@@ -8,28 +9,27 @@ export default function WeatherInfo(props) {
       <div className="row wrap">
         <div className="col-4 current-props.data bottom-sections">
           <h2>{props.data.city}</h2>
-          <p>
+          <p className="date">
             <FormattedDate date={props.data.date} />
           </p>
           <p className="description">{props.data.description}</p>
           <div className="row temperature">
             <div className="col-4">
-              <h3>{Math.round(props.data.temperature)}</h3>
+              <span className="main-temperature">
+                {Math.round(props.data.temperature)}
+              </span>
               <span className="units-symbol">°C</span>
             </div>
             <div className="col-8">
               <ul>
                 <li>Humidity: {props.data.humidity}%</li>
-                <li>Wind: {props.data.wind} km/h</li>
+                <li>Wind: {Math.round(props.data.wind)} km/h</li>
               </ul>
             </div>
           </div>
-          <img
-            src={props.data.icon}
-            alt={props.data.description}
-            className="weather-icon"
-            width="150"
-          />
+          <div className="weather-icon">
+            <WeatherIcon code={props.data.icon} />
+          </div>
         </div>
         <div className="col-4  forecast-column bottom-sections">
           <h4>5 DAY FORECAST</h4>
